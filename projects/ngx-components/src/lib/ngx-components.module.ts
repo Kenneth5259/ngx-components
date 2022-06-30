9f98c7ab9ac4432121cf2ad21ec600f3cf52335f
Kenneth Carroll <kenneth.james.carroll@gmail.com>
2022-06-29 21:51:12 -0400

    Generated empty library

---
+++ projects/ngx-components/src/lib/ngx-components.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NgxComponentsComponent } from './ngx-components.component';
+
+
+
+@NgModule({
+  declarations: [
+    NgxComponentsComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    NgxComponentsComponent
+  ]
+})
+export class NgxComponentsModule { }
